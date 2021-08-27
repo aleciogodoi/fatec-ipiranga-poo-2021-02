b0ca6d3f7e0e449a7fa05931c47955f5fd8e0ad7
Alécio <alecio.godoi@ymail.com>
2021-08-27 19:24:16 -0300

    Aula 05

---
+++ Aula-05/src/Aluno.java
@@ -0,0 +1,10 @@
+public class Aluno {
+	// Atributos
+	String nome;
+	String matricula;
+	Double nota1;
+	Double nota2;
+	Double notaReavaliacao;
+	
+	
+}
---
+++ Aula-05/src/TesteAluno.java
@@ -0,0 +1,5 @@
+public class TesteAluno {
+	public static void main(String[] args) {
+
+	}
+}
