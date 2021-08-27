6f33e32d4276028184bfc8e0f3b9ede6aa05d506
Alécio <alecio.godoi@ymail.com>
2021-08-27 19:57:16 -0300

    Aula 05

--- Aula-05/src/Aluno.java
+++ Aula-05/src/Aluno.java
@@ -5,6 +5,17 @@ public class Aluno {
 	Double nota1;
 	Double nota2;
 	Double notaReavaliacao;
-	
-	
+	// Construtor vazio
+	Aluno(){
+	}
+	// Construtor com parametros
+	Aluno(String nome, String matricula,
+		  Double nota1, Double nota2,
+		  Double notaReavaliacao) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+		this.notaReavaliacao = notaReavaliacao;
+	}
 }
