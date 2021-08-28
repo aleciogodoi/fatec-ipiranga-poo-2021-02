ee480b5f913767dc2d1f946756fe4032e3126d6b
Alécio <alecio.godoi@ymail.com>
2021-08-27 22:19:49 -0300

    Aula 05

--- Aula-05/src/Aluno.java
+++ Aula-05/src/Aluno.java
@@ -2,27 +2,31 @@ public class Aluno {
 	// Atributos
 	String nome;
 	String matricula;
-	Double nota1;
-	Double nota2;
-	Double notaReavaliacao;
+	private double nota1;
+	private double nota2;
+	private double notaReavaliacao;
+	private boolean aprovado; 
+	
 	// Construtor vazio
 	Aluno(){
 	}
 	// Construtor com parametros
 	Aluno(String nome, String matricula,
-		  Double nota1, Double nota2,
-		  Double notaReavaliacao) {
+		  double nota1, double nota2,
+		  double notaReavaliacao) {
 		this.nome = nome;
 		this.matricula = matricula;
-		this.nota1 = nota1;
-		this.nota2 = nota2;
-		this.notaReavaliacao = notaReavaliacao;
+		setNota1(nota1);
+		setNota2(nota2);
+		setNotaReavaliacao(notaReavaliacao);
+		aprovado = (calcularMediaFinal() >= 6);
 	}
 	@Override
 	public String toString() {
 		return "Aluno [Nome=" + nome + ", Matricula=" + matricula + ", Nota1=" + nota1 + ", Nota2=" + nota2
 				+ ", Nota Reavaliação=" + notaReavaliacao + ", Média=" + calcularMedia()
-				+ ", Média Final=" + calcularMediaFinal() + "]";
+				+ ", Média Final=" + calcularMediaFinal() 
+				+ ", Aprovado="+aprovado+ "]";
 	}
 	
 	public double calcularMedia() {
@@ -34,5 +38,45 @@ public class Aluno {
 			return calcularMedia();
 		else
 			return ( calcularMedia() + notaReavaliacao ) / 2;
+	}
+	
+	public boolean getAprovado() {
+		return aprovado;
+	}
+
+	public void setNota1(double n1) {
+		if (n1>=0 && n1<=10) {
+			nota1 = n1;
+			aprovado = (calcularMediaFinal() >= 6);
+		} else {
+			System.out.println("Nota 1="+n1+ " - Valor Inválido!");
+		}
+	}
+
+	public void setNota2(double n2) {
+		if (n2>=0 && n2<=10) {
+			nota2 = n2;
+			aprovado = (calcularMediaFinal() >= 6);
+		} else {
+			System.out.println("Nota 2="+n2+ " - Valor Inválido!");
+		}
+	}
+
+	public void setNotaReavaliacao(double n) {
+		if (n>=0 && n<=10) {
+			notaReavaliacao = n;
+			aprovado = (calcularMediaFinal() >= 6);
+		} else {
+			System.out.println("Nota Reavaliação="+n+ " - Valor Inválido!");
+		}
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public double getNotaReavaliacao() {
+		return notaReavaliacao;
 	}
 }
--- Aula-05/src/TesteAluno.java
+++ Aula-05/src/TesteAluno.java
@@ -1,14 +1,17 @@
 public class TesteAluno {
 	public static void main(String[] args) {
 		Aluno aluno01 = new Aluno("Alana", "1010-1", 10.0, 10.0, 0.0);
-		Aluno aluno02 = new Aluno("Matheus", "1010-2", 8.0, 3.0, 7.0);
-		
+		Aluno aluno02 = new Aluno("Matheus", "1010-2", 8.0, 3.0, 3.0);
+
 		System.out.println(aluno01.toString());
 		System.out.println(aluno01.calcularMedia());	
 		
+		aluno02.setNotaReavaliacao(7.0);
 		System.out.println(aluno02.toString());
 		System.out.println("Média: "+aluno02.calcularMedia());
 		System.out.println("Média Final: "+aluno02.calcularMediaFinal());
+		System.out.println("Aprovado:"+aluno02.getAprovado());
+		System.out.println(aluno02.getNota1());
 		
 	}
 }
