10c3865b906e3c03e5325f28d2f84e13a753e8da
Alécio <alecio.godoi@ymail.com>
2021-09-03 21:08:16 -0300

    Aula 06

--- Aula-06/src/Exercicio_01/Main.java
+++ Aula-06/src/Exercicio_01/Main.java
@@ -1,13 +1,12 @@
 package Exercicio_01;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Funcionario func01 = new Funcionario("Pedro", "1212121-09", 5234.21);
 		System.out.println(func01);
 		
 		Gerente ger01 = new Gerente("Alex", "22212121-87", 9765.87, 1234);
 		System.out.println(ger01);
+		System.out.println(ger01.autentica(0));
 	}
-
 }
