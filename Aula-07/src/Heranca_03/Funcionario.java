1c7f9cdfd38c5403f9e3d6a8e6d71196ec154c58
Alécio <alecio.godoi@ymail.com>
2021-09-17 19:13:13 -0300

    Exercício

--- Aula-07/src/Heranca_03/Funcionario.java
+++ Aula-07/src/Heranca_03/Funcionario.java
@@ -1,5 +1,35 @@
 package Heranca_03;
 
-public class Funcionario {
+public class Funcionario extends Pessoa{
+	private Data dtAdmissao; 
+	private float salario;
+	
+	public Funcionario(String nome, String identidade, Data dtNascimento, 
+						Data dtAdmissao, float salario) {
+		super(nome, identidade, dtNascimento);
+		this.dtAdmissao = dtAdmissao;
+		this.salario = salario;
+	}
+
+	public Data getDtAdmissao() {
+		return dtAdmissao;
+	}
+
+	public void setDtAdmissao(Data dtAdmissao) {
+		this.dtAdmissao = dtAdmissao;
+	}
+
+	public float getSalario() {
+		return salario;
+	}
+
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario [dtAdmissao=" + dtAdmissao + ", salario=" + salario + "]";
+	}
 
 }
