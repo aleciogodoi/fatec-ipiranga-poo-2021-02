a2ac7ed92a7137a94611466180fe17c7f75a671e
Alécio <alecio.godoi@ymail.com>
2021-11-05 22:27:40 -0300

    Aula 14

--- Aula-14/src/Teste.java
+++ Aula-14/src/Teste.java
@@ -1,11 +1,40 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		Connection conn = null;    // objeto do tipo Connection
+		Scanner sc = new Scanner(System.in);
+		int depto = 0;
+		Connection conn = null;    // objeto do tipo Java Connection
 		Conexao conexao = new Conexao(); // objeto do tipo Conexao
 		conn = conexao.conectar();	// abrindo uma conexão com o BD
+		
+		do {
+			System.out.print("Id Depto: ");
+			depto = sc.nextInt();
+			
+			// string com comando Select
+			String sql = "Select idDepto, nomeDepto From Departamento Where idDepto="+depto;
+			try {
+				// Criar objeto para execução de comandos SQL
+				Statement comandoSQL = conn.createStatement();
+				
+				// Executar comando SQL e guardar o resultado
+				ResultSet rs = comandoSQL.executeQuery(sql);
+				
+				while (rs.next()) { // lê uma linha do resultado
+					System.out.println(rs.getInt("idDepto")+" - "+rs.getString("nomeDepto"));
+				}				
+			} catch (SQLException ex) {
+				System.out.println("Problema Execução SQL - "+sql);
+				ex.printStackTrace();
+			}
+		} while (depto!=0);
+		
 		conexao.fechar(conn); // fechando a conexão com o BD
 	}
 	
