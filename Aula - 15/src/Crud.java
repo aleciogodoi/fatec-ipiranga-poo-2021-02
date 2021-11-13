a3bc740600e39238c730d075f8f8b04103925d89
Alécio <alecio.godoi@ymail.com>
2021-11-12 21:07:38 -0300

    Aula 15

--- Aula - 15/src/Crud.java
+++ Aula - 15/src/Crud.java
@@ -1,4 +1,6 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
@@ -12,6 +14,20 @@ public class Crud {
 		int opcao=0;
 		do {
 			opcao = menu();
+			switch (opcao) {
+			case 1:
+				incluir();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				listar();
+				break;
+			}
 		} while (opcao != 0 );
 		System.out.println("F I M");
 	}
@@ -27,9 +43,35 @@ public class Crud {
 					+ "0 - Sair\n"
 					+ "Ecolha uma opção: ";
 		System.out.print(menu);
-		System.out.println();
 		opcao = sc.nextInt();
+		System.out.println();
+
 		return opcao;
+	}
+	
+	public static void listar () {
+		String sql = "Select idUsuario,Nome,Email,Telefone,Endereco From Usuario";
+		conn = conexao.conectar();
+		try {
+			comandoSQL = conn.createStatement(); // Objeto para execução dos comandos SQL
+			ResultSet rs = comandoSQL.executeQuery(sql);  // Guardo os resultados em um Objeto Java ResultSet
+			while (rs.next()) 
+				System.out.println(	 rs.getInt("idUsuario")+", "
+									+rs.getString("Nome")+", "
+									+rs.getString("Email")+", "
+									+rs.getString("Telefone")+", "
+									+rs.getString("Endereco") );
+			
+		} catch (SQLException ex) {
+			System.out.println("Problemas na Execução do comando: "+sql);
+			ex.printStackTrace();
+		} finally {
+			conexao.fechar(conn);
+		}
+	}
+	
+	public static void incluir() {
+		
 	}
 
 }
